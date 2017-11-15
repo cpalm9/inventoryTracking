29b498be78a10849882011d36c39efb29280d4f4
Chris Palmer <christian_palmer@byu.edu>
2017-11-15 09:55:22 -0700

    working on the products

--- inventory_tracking/inventory_tracking/settings.py
+++ inventory_tracking/inventory_tracking/settings.py
@@ -143,8 +143,12 @@ WSGI_APPLICATION = 'inventory_tracking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'InvTracking',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
